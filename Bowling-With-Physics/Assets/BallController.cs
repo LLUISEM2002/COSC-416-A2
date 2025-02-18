1d2dcaa26a0a03a65553b42865c2c8acdbde12b6
Lluis Escolano <113066894+LLUISEM2002@users.noreply.github.com>
2025-02-17 22:00:57 -0800

    now ball attached to player

--- Bowling-With-Physics/Assets/BallController.cs
+++ Bowling-With-Physics/Assets/BallController.cs
@@ -4,24 +4,25 @@ public class BallController : MonoBehaviour
 {
 private bool isBallLaunched;
 [SerializeField] private float force = 1f;
+[SerializeField] private Transform ballAnchor;
 [SerializeField] private InputManager inputManager;
 
 private Rigidbody ballRB;
 
 void Start(){
-//Grabbing a reference to RigidBody
-ballRB = GetComponent<Rigidbody>();
-
-// Add a listener to the OnSpacePressed event.
-// When the space key is pressed the
-// LaunchBall method will be called.
-inputManager.OnSpacePressed.AddListener(LaunchBall);
+    ballRB = GetComponent<Rigidbody>();
+    inputManager.OnSpacePressed.AddListener(LaunchBall);
+    transform.parent = ballAnchor;
+    transform.localPosition = Vector3.zero;
+    ballRB.isKinematic = true;
 }
 
 
 private void LaunchBall(){
     if(isBallLaunched) return;
     isBallLaunched = true;
+    transform.parent = null;
+    ballRB.isKinematic = false;
     ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
 }
  }
